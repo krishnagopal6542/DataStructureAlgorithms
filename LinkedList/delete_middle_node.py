f112a52ac191c58599a6ce3b8b1ebc39f73ed714
Krishna Gopal Ojha <krishna@Krishnas-MacBook-Air.local>
2026-08-28 11:35:10 +0530

    LL aded

---
+++ LinkedList/delete_middle_node.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def delete_middle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next is None:
+            return None
+
+        slow = head
+        fast = head.next.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
+
+        return head
